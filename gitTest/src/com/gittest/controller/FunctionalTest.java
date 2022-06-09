005bb48b20fb92413d652041eb1b6d215b334d10
Do-Yeoung Kim <pinataman01@gmail.com>
2022-06-09 15:50:05 +0900

    05 printTest에서 출력구문 삭제함

--- gitTest/src/com/gittest/controller/FunctionalTest.java
+++ gitTest/src/com/gittest/controller/FunctionalTest.java
@@ -3,7 +3,6 @@ package com.gittest.controller;
 public class FunctionalTest {
 
 		public void printTest() {
-			System.out.println("gitprint");
 			String name="test"; //ctrl + s, 저장한다고 해서 곧바로 git에 의해 관리가 되는 건 아님! commit을 해야만 함!
 			//그저 modifed상태로서, unstaged changes임을 확인할 수 있다 -
 		}
